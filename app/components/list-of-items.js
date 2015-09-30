eae7faefc2f75b208183fa4fdf986527a25b1472
Kirill Matrosov <kdmatrosov@gmail.com>
2015-09-30 22:23:03 +0300

    Пример компонента с выводом в цикле. Данные хранятся в компоненте.

--- app/components/list-of-items.js
+++ app/components/list-of-items.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  items: ['item1', 'item2', 'item3'],
+  actions:
+  {
+    addNewItem: function()
+    {
+      let newItem = this.get('newItem');
+      this.get('items').pushObject(newItem);
+    }
+  }
 });
