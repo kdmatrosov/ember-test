4cf68a8c3549cfaf15a1764a7391c544fb615552
Kirill Matrosov <kdmatrosov@gmail.com>
2015-09-30 23:07:28 +0300

    Вызов метода роута из компонента

--- app/components/list-of-items.js
+++ app/components/list-of-items.js
@@ -8,6 +8,9 @@ export default Ember.Component.extend({
     {
       let newItem = this.get('newItem');
       this.get('items').pushObject(newItem);
+    },
+    saveItems: function () {
+      this.sendAction('action', this.get('items')); // слово "action" - просто метка на метод. Есть здесь и в файле с вызовом компонента
     }
   }
 });
--- app/routes/farm.js
+++ app/routes/farm.js
@@ -4,5 +4,10 @@ export default Ember.Route.extend({
     model: function()
     {
         return ['item2', 'item4', 'item6'];
-    }
+    },
+      actions: {
+        storeItems: function (items) {
+         console.log(items.join(', ')+ ' were saved');
+        }
+      }
 });
