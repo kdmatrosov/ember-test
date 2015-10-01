f7ec20989c189289b28317f5ff25d0858e263411
Kirill Matrosov <kdmatrosov@gmail.com>
2015-10-01 08:33:10 +0300

    Создана запись в сторе на тип фермы

--- app/routes/farm.js
+++ app/routes/farm.js
@@ -3,10 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	
 	  activate() { // Как я понял, этому методу очень важно быть первым определенным в роутере или он кладет всё нахер
-			//console.log(store.findAll('farm'));
+			
 	  },
 	  model: function()
 	  {
+		  this.store.push(
+		  {
+			data: [{
+				  id: 1,
+					type: 'farm',
+				  attributes:
+				  {
+					name: 'Мой край, моя деревня' ,
+					address: 'От второй звезы напрво и до самого утра'  
+				  }
+			}]
+		  });
 		return ['item2', 'item4', 'item6'];
 	  },
       actions: {
